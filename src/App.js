ccefddd8b4ff7feced41f21d65a69ed4f418c4de
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-08-07 21:13:50 +0530

    Modify  src/App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from './component/Header';
+import Rockets from './component/Rockets';
+import Missions from './component/Missions';
+import Dragons from './component/Dragons';
+import Profile from './component/Profile';
 
 function App() {
-
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/dragons" element={<Dragons />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
