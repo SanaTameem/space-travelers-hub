e6e2a939841df05cb0e4b565b49dd8affcd81ffa
Peter-Kibet <peter.kibet@hotmail.com>
2023-08-08 19:09:13 +0300

    Add my dragons in the My profile section

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,14 +4,12 @@ import '../Styles/Profile.css';
 function Profile() {
   return (
     <div className="myprofile">
-      <h1 className="profileHeading">My Profile</h1>
       <section className="profile">
-        <div className="left-myMissions">
-          <h2>My Missions</h2>
-        </div>
-        <div className="right-myRockets">
-          <h2>My Rockets</h2>
-        </div>
+        <h2>My Missions</h2>
+
+        <h2>My Rockets</h2>
+
+        <h2>My Dragons</h2>
       </section>
     </div>
   );
