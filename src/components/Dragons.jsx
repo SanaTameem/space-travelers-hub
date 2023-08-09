e9006a82c18ff6d652435ef3aac9b2abac896be9
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-08-09 20:12:44 +0530

    Modify src/components/Dragons.jsx file

--- src/components/Dragons.jsx
+++ src/components/Dragons.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import '../Styles/Dragons.css';
 import { fetchDragons } from '../redux/dragons/dragonsSlice';
 
@@ -9,9 +9,25 @@ function Dragons() {
     dispatch(fetchDragons());
   }, [dispatch]);
 
+  const dragonsData = useSelector((state) => state.dragon.dragonsData);
+
   return (
     <div className="dragons-main-container">
-      Dragons
+      {dragonsData.map((dragon) => (
+        <div className="dragon-container" key={dragon.id}>
+          <div className="dragon-img-div">
+            <img src={dragon.flickr_images} alt={dragon.type} className="dragon-img" />
+          </div>
+          <div className="dragon-data">
+            <h2 className="dragon-title">{dragon.name}</h2>
+            <p className="dragon-info">
+              <span className="reserved-span">Reserved</span>
+              {dragon.description}
+            </p>
+            <button type="button" className="reserve-btn">Reserve Dragon</button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
