8084129cdbad01e237e0ac8c616cb93101edcbb3
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-08-09 19:40:24 +0430

    Update  src/components/Rockets.jsx file

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import '../Styles/Rockets.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchRockets } from '../redux/rockets/rocketsSlice';
 
 function Rockets() {
@@ -8,9 +8,24 @@ function Rockets() {
   useEffect(() => {
     dispatch(fetchRockets());
   }, [dispatch]);
+  const rocketData = useSelector((state) => state.rocket.rocketsData);
   return (
     <div className="rockets-main-container">
-      Rockets
+      {rocketData.map((rocket) => (
+        <div className="rocket-container" key={rocket.id} id={rocket.id}>
+          <div className="rocket-img-div">
+            <img src={rocket.flickr_images} alt={rocket.type} className="rocket-img" />
+          </div>
+          <div className="rocket-data">
+            <h2 className="rocket-title">{rocket.name}</h2>
+            <p className="rocket-info">
+              {/* <span className="reserved-span">Reserved</span> */}
+              {rocket.description}
+            </p>
+            <button type="button" className="reserve-btn">Reserve Rocket</button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
